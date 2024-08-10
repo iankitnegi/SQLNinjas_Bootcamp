5c943ca62975d4402ff530cd666d96916d43d026
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-08-10 12:58:45 +0530

    Add files via upload

--- sql_course_ch01.sql
+++ sql_course_ch01.sql
@@ -1,32 +1,60 @@
+#1
 SELECT *
 FROM movies;
 
+#2
 SELECT title, industry
 FROM movies;
 
+#3
 SELECT title, industry
 FROM movies
 WHERE industry="Bollywood";
 
+#4
 SELECT COUNT(*)
 FROM movies
 WHERE industry="Bollywood";
 
+#5
 SELECT COUNT(*)
 FROM movies
 WHERE industry="Hollywood";
 
+#6
 SELECT DISTINCT industry
 FROM movies;
 
+#7
 SELECT *
 FROM movies
 WHERE title LIKE "%Thor%";
 
+#8
 SELECT *
 FROM movies
 WHERE title LIKE "%America%";
 
+#9
 SELECT *
 FROM movies
 WHERE studio="";
+
+# Print all movie titles and release year for all Marvel Studios movies
+SELECT title, release_year
+FROM movies;
+
+# Print all movies that have Avenger in their name
+SELECT *
+FROM movies
+WHERE title LIKE "%Avengers%";
+
+# Print the year in which "The Godfather" move was released
+SELECT release_year
+FROM movies
+WHERE title= "The Godfather";
+
+# Print all distinct movie studios on Bollywood industry
+SELECT DISTINCT studio
+FROM movies
+WHERE industry="Bollywood";
